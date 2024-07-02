abad91ad8a4ed8e05ab8cbcb65d746bf93879ab4
xB0bon <pan.bobon@gmail.com>
2024-07-02 22:55:34 +0200

    First Lesson

---
+++ Basics/First Lesson/First Lesson/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// declare string variable and assign my name to the variable
+string MyName = "Peter";
+
+// use the variable
+Console.WriteLine(MyName);
+
+// change the variable
+MyName = "John";
+
+Console.WriteLine(MyName);
