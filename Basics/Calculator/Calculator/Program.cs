7b944690bbf4d786b8e38af63a8e11c8fee8e2c2
xB0bon <pan.bobon@gmail.com>
2024-07-02 23:39:44 +0200

    .

--- Basics/Calculator/Calculator/Program.cs
+++ Basics/Calculator/Calculator/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter Your name: ");
+string name = Console.ReadLine();
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine($"Hello, {name}!");
--- Basics/Calculator/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Basics/Calculator/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6707a1e44c086322c26bae0d39789df7f14d36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abad91ad8a4ed8e05ab8cbcb65d746bf93879ab4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
