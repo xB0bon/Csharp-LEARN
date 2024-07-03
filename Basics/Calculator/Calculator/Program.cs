aceda011ddeec05257b0f74c6abd3c6b2959e8f4
xB0bon <pan.bobon@gmail.com>
2024-07-03 17:25:43 +0200

    .

--- Basics/Calculator/Calculator/Program.cs
+++ Basics/Calculator/Calculator/Program.cs
@@ -12,7 +12,7 @@ double secondNumber;
 Console.WriteLine("Enter first number: ");
 // Replace - zamienia dany znak i inny wybrany
 string input = Console.ReadLine().Replace(".", ",");
-firstNumber = double.Parse(input);
+firstNumber = Convert.ToDouble(input);
 
 // zamiast replace można zastosować CultureInfo.InvariantCulture
 //np.
@@ -26,6 +26,7 @@ secondNumber = double.Parse(input);
 
 // obliczanie
 double sum = firstNumber + secondNumber;
+sum = Math.Round(sum, 3);
 
 // wyświetlanie wyniku
 Console.WriteLine($"{firstNumber.ToString(CultureInfo.InvariantCulture)} + {secondNumber.ToString(CultureInfo.InvariantCulture)} = {sum.ToString(CultureInfo.InvariantCulture)}");
