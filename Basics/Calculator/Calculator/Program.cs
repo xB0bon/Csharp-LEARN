3562470ac24800ca5299d5c105e7f44b183104fa
xB0bon <pan.bobon@gmail.com>
2024-07-03 15:07:28 +0200

    calc

--- Basics/Calculator/Calculator/Program.cs
+++ Basics/Calculator/Calculator/Program.cs
@@ -1,5 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Enter Your name: ");
-string name = Console.ReadLine();
 
-Console.WriteLine($"Hello, {name}!");
+
+// tworzenie zmiennych
+
+using System.Globalization;
+
+double firstNumber;
+double secondNumber;
+
+// pierwsza liczba
+Console.WriteLine("Enter first number: ");
+// Replace - zamienia dany znak i inny wybrany
+string input = Console.ReadLine().Replace(".", ",");
+firstNumber = double.Parse(input);
+
+// zamiast replace można zastosować CultureInfo.InvariantCulture
+//np.
+// firstNumber = double.Parse(input, CultureInfo.InvariantCulture);
+
+
+// drugi znak
+Console.WriteLine("Enter second number: ");
+input = Console.ReadLine().Replace(".", ",");
+secondNumber = double.Parse(input);
+
+// obliczanie
+double sum = firstNumber + secondNumber;
+
+// wyświetlanie wyniku
+Console.WriteLine($"{firstNumber.ToString(CultureInfo.InvariantCulture)} + {secondNumber.ToString(CultureInfo.InvariantCulture)} = {sum.ToString(CultureInfo.InvariantCulture)}");
