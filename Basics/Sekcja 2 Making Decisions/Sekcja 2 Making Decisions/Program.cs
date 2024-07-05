bb55d050906a1e4b4c30685c6439044d82b2dcc4
xB0bon <pan.bobon@gmail.com>
2024-07-05 16:09:57 +0200

    xd

---
+++ Basics/Sekcja 2 Making Decisions/Sekcja 2 Making Decisions/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool isRainy = true;
+bool hasUmbrella = true;
+
+if (isRainy && !hasUmbrella)
+{   
+    Console.WriteLine("It's raining, but you don't have an umbrella. You'll have to take an umbrella.");
+}
+
+else if (isRainy && hasUmbrella)
+{
+    Console.WriteLine("It's raining and you have an umbrella. You can enjoy the weather.");
+}
+
+else if (!isRainy)
+{
+    Console.WriteLine("It's not raining, so you can enjoy the weather without an umbrella.");
+}
---
+++ Basics/Sekcja 3 Loops/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine(i);
+    Thread.Sleep(1000);
+}
+
+Console.WriteLine("Countdown completed!");
