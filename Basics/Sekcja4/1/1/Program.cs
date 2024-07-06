62195364a561f3352643f4a9638e640bf515ec4d
xB0bon <pan.bobon@gmail.com>
2024-07-06 18:28:56 +0200

    6/7/2024

---
+++ Basics/Sekcja4/1/1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Method Declaration
+int AddTwoValues(int number1, int number2)
+{
+    int result = number1 + number2;
+    return result;
+}
+
+
+Console.WriteLine("Please Enter first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Please Enter second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+// Calling the method "Add"
+int myResult = AddTwoValues(firstNumber, secondNumber);
+
+Console.WriteLine("Result is: " + myResult);
+
