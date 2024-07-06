6fbe6f454e0a4006b79f4d046f157c54c1fbc752
xB0bon <pan.bobon@gmail.com>
2024-07-06 22:41:18 +0200

    06/07/2024

---
+++ Basics/Sekcja4/UdemyExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace UdemyExercise;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] temperatures = { 45.5, 32.0, 1.0, 4.0, 6.0, 8.0, 3.0, 11.0, -7.0 };
+        
+        PrintAverage(temperatures);
+    }
+    
+    static void PrintAverage(double[] temperatures)
+    {
+        double average = CalculateAverage(temperatures);
+
+        Console.WriteLine("The average temperature is: " + average);
+    }
+
+    static double CalculateAverage(double[] temperatures)
+    {
+        double sum = 0;
+        foreach (double temperature in temperatures)
+        {
+            sum += temperature;
+        }
+        
+        return (sum / temperatures.Length);
+    }
+}
