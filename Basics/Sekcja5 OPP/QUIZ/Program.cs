8a8e1680091c8b26b75cdb5e5d32efc17291b514
xB0bon <pan.bobon@gmail.com>
2024-07-12 23:30:17 +0200

    .

---
+++ Basics/Sekcja5 OPP/QUIZ/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace QUIZ;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Basics/Sekcja5 OPP/QUIZ/Question.cs
@@ -0,0 +1,10 @@
+﻿namespace QUIZ;
+
+internal class Question
+{
+    public string QuestionText { get; set; }
+    
+    public string[] PossibleAnswers { get; set; }
+    
+    public int CorrectAnswerIndex { get; set; }
+}
