e771eb29538b9eda5d36d87761c4176f8645842d
xB0bon <pan.bobon@gmail.com>
2024-07-14 22:25:50 +0200

    .

---
+++ Basics/Sekcja6 Collections/Collection1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Collection1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int> {1, 2, 3};
+
+        foreach (var number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
