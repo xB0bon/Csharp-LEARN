80cff5de2d8bb311882a430fa469145876174f92
xB0bon <pan.bobon@gmail.com>
2024-07-16 17:53:58 +0200

    .

---
+++ Basics/Sekcja6 Collections/Collections4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Collections4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, string> employees = new Dictionary<int, string>();
+        
+        employees.Add(1, "John Doe");
+        employees.Add(2, "Jane Smith");
+        employees.Add(3, "Michael Johnson");
+        employees.Add(4, "Emily Davis");
+        
+        foreach (var employee in employees)
+        {
+            Console.WriteLine($"Employee ID: {employee.Key}, Employee Name: {employee.Value}");
+        }
+    }
+}
