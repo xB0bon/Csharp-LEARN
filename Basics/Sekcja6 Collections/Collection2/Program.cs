b812e9d52bbbc5dd9644531a9d09d48ab2c1eb27
xB0bon <pan.bobon@gmail.com>
2024-07-16 15:30:44 +0200

    .

--- Basics/Sekcja6 Collections/Collection2/Program.cs
+++ Basics/Sekcja6 Collections/Collection2/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         List<string> colors = new List<string>() {"Red", "Green", "Yellow", "Blue", "Purple", "Aquamarine", "Orange"};
-        List<int> numbers = new List<int>() {4,6,2,22,7,2,1,8,4,3,2,11};
+        List<int> numbers = new List<int>() {4,6,2,22,7,2,1,8,4,3,2,11, 15, 16, 101};
         
         Console.WriteLine("Original List: ");
         foreach (var number in numbers)
@@ -18,5 +18,32 @@ class Program
         {
             Console.Write(number + ",");
         }
+
+
+        Predicate<int> isGreaterThanTen = x => x > 10;
+        Console.WriteLine("\nNumbers greater than 10");
+        
+        //foreach (var number in numbers.FindAll(x => x > 10))
+        // lub
+        foreach (var number in numbers.FindAll(isGreaterThanTen))
+        {
+            Console.Write(number+",");
+        }
+
+        bool numberisGreaterThanHundred = numbers.Any(number => number > 100);
+
+        if (numberisGreaterThanHundred)
+        {
+            Console.WriteLine("\nthere is a number greater than 100 in this list: ");
+            foreach (var number in numbers.FindAll(number => number > 100))
+            {
+                Console.Write(number+",");
+            }
+        }
+        
+        else 
+            Console.WriteLine("\nthere are no numbers greater than 100 in this list");
+
+        
     }
 }
