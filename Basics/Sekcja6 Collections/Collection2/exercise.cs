01619cab6c29695dddc1dffb92eaf11819316181
xB0bon <pan.bobon@gmail.com>
2024-07-16 15:30:29 +0200

    .

---
+++ Basics/Sekcja6 Collections/Collection2/exercise.cs
@@ -0,0 +1,14 @@
+﻿namespace Collection2;
+
+public class exercise
+{
+    public exercise()
+    {
+        List<int> exerciseList = new List<int>() {1,2,3,4,5,6,7,8,9};
+
+        bool numbersGreaterThanFive = exerciseList.Any(number => number > 5);
+        
+
+        Console.WriteLine(numbersGreaterThanFive);
+    }
+}
