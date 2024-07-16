8eecdf5ba3eb2c719dc20d891e72681e7d311652
xB0bon <pan.bobon@gmail.com>
2024-07-16 15:31:21 +0200

    .

---
+++ Basics/Sekcja6 Collections/Collections3/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Collections3;
+
+public class Product
+{
+    public string Name { get; set; }
+    
+    // decimal jest lepsze do cen
+    public decimal Price { get; set; }
+    
+    
+    public Product(string name, decimal price)
+    {
+        Name = name;
+        Price = price;
+    }
+}
---
+++ Basics/Sekcja6 Collections/Collections3/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Collections3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> products = new List<Product>() {new Product("Eggs", 12m),
+            new Product("bananas", 5.5m),
+            new Product("Milk", 3.4m),
+            new Product("Apples", 7m),
+            new Product("Oranges", 4m),
+            new Product("Cheese", 10m),
+            new Product("Bread", 2m),
+            new Product("Fish", 15m),
+            new Product("Cereal", 8m),
+            new Product("Chicken", 18m)
+        };
+
+        foreach (var product in products)
+        {
+            Console.WriteLine($"Product name: {product.Name}, Product price: {product.Price} zł");
+        }
+
+        
+        
+        List<Product> expensiveProducts = products.Where(x => x.Price > 10).ToList();
+        
+        Console.WriteLine("Expensive products: ");
+        //foreach (var product in products.Where(x => x.Price> 10))
+        foreach (var product in expensiveProducts)
+        {
+            Console.WriteLine($"Product name: {product.Name}, Product price: {product.Price} zł");
+        }
+    }
+}
