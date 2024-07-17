b5ffe02818d764718b948e600321a23b9f147824
xB0bon <pan.bobon@gmail.com>
2024-07-17 22:13:17 +0200

    .

---
+++ Basics/Sekcja6 Collections/Collecions5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Collecions5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>() {1,2,3};
+        foreach (var i in list)
+        {
+            Console.Write(i + " ");
+        }
+       
+        
+
+    }
+}
