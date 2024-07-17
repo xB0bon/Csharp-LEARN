f69f8c10f24a3da690606298c438fe34717d12f3
xB0bon <pan.bobon@gmail.com>
2024-07-17 22:27:18 +0200

    Exercise

--- Basics/Sekcja6 Collections/Collecions5/Program.cs
+++ Basics/Sekcja6 Collections/Collecions5/Program.cs
@@ -2,15 +2,26 @@
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        List<int> list = new List<int>() {1,2,3};
-        foreach (var i in list)
-        {
-            Console.Write(i + " ");
-        }
-       
+        Dictionary<int, Student> students = new Dictionary<int, Student>();
         
+        students.Add(1, new Student("John", 85));
+        students.Add(2, new Student("Alice", 90));
+        students.Add(3, new Student("Bob", 78));
+        
+        // Loop through the dictionary and print student names and grades
+        foreach (var student in students)
+        {
+            Console.WriteLine($"Name: {student.Value.Name}," +
+                              $" Id: {student.Key}," +
+                              $" Grade: {student.Value.Grade}");
+        }
+        
+        
+
+
 
     }
 }
---
+++ Basics/Sekcja6 Collections/Collecions5/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace Collecions5;
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Grade { get; set; }
+
+    public Student(string name, int grade)
+    {
+        Name = name;
+        Grade = grade;
+    }
+}
