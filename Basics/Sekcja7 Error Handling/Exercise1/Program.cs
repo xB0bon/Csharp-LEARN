d047a0bb44ef67d61b98750bc4bf543b27e1feac
xB0bon <pan.bobon@gmail.com>
2024-07-18 21:22:22 +0200

    .

---
+++ Basics/Sekcja7 Error Handling/Exercise1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercise1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int result = 0;
+        int num1 = 5;
+        int num2 = 0;
+
+        try
+        {
+            result = num1 / num2;
+            Console.WriteLine(result);
+        }
+        
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        finally
+        {
+            Console.WriteLine("Result: " + result);
+        }
+        
+        
+    }
+}
