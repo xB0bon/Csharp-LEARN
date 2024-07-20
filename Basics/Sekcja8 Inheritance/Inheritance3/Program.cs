75b77367c5a4d302787d20b6e8b4cfb896efa53c
xB0bon <pan.bobon@gmail.com>
2024-07-20 22:38:29 +0200

    .

---
+++ Basics/Sekcja8 Inheritance/Inheritance3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Inheritance3;
+
+public class Person
+{
+    public string Name { get; set; }
+    public Person(string name)
+    {
+        Name = name;
+        Console.WriteLine($"Person constructor: {Name}");
+    }
+}
+
+public class Employee : Person
+{
+    public int ID { get; set; }
+
+    public Employee(string name, int id) : base(name)
+    {
+        ID = id;
+        Console.WriteLine($"Employee constructor: {name}, ID: {ID}");
+    }
+
+
+    public class Exercise
+    {
+        public void PrintMessages()
+        {
+            Employee e1 = new Employee("John Doe", 123);
+        }
+    }
+}
