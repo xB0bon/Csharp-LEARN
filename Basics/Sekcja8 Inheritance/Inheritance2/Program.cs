9f41b4a555f872190373649c70fb41f3c6720377
xB0bon <pan.bobon@gmail.com>
2024-07-20 21:38:22 +0200

    .

---
+++ Basics/Sekcja8 Inheritance/Inheritance2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Inheritance2;
+
+class Program
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Animal sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            base.MakeSound();
+            Console.WriteLine("Dog barks");
+        }
+    }
+
+    public class Exercise
+    {
+        public void PrintSounds()
+        {
+            Dog dog = new Dog();
+            dog.MakeSound(); // Output: Animal sound, Dog barks
+        }
+    }
+}
