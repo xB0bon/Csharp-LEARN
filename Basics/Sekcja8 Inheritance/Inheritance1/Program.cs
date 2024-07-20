cb8238b5eed06ec781915539625e26438d185e11
xB0bon <pan.bobon@gmail.com>
2024-07-20 21:33:44 +0200

    .

---
+++ Basics/Sekcja8 Inheritance/Inheritance1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Inheritance1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog pies = new Dog();
+        Cat kot = new Cat();
+        pies.MakingSound();
+        kot.MakingSound();
+    }
+}
+
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+
+    
+    // VIRTUAL umożliwia nadpisywanie tej metody w klasasach potomnych
+    public virtual void MakingSound()
+    {
+        Console.WriteLine("Making a sound...");
+    }
+    
+}
+
+class Dog : Animal
+{
+    public override void MakingSound()
+    {
+        base.MakingSound();
+        Console.WriteLine("Woof!");
+    }
+}
+
+class Colie : Dog
+{
+    public void GoingNuts()
+    {
+        Console.WriteLine("Going for a nutty walk...");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakingSound()
+    {
+        base.MakingSound();
+        Console.WriteLine("Meow!");
+    }
+}
