93d6f635bb0faee25bdb3d1891fe8c5e222e0351
xB0bon <pan.bobon@gmail.com>
2024-07-22 00:18:24 +0200

    .

---
+++ Basics/Sekcja9/E2/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace E2;
+
+
+public interface IPaymentProcessor
+{
+    void ProcessPayment(decimal amount);
+}
+
+public class CreditCard : IPaymentProcessor
+{
+    public void ProcessPayment(decimal amount)
+    {
+        Console.WriteLine($"Credit card payment processed for {amount} $");
+    }
+}
+
+public class PaypalProcessor : IPaymentProcessor
+{
+    public void ProcessPayment(decimal amount)
+    {
+        Console.WriteLine($"PayPal payment processed for {amount} $");
+    }
+}
+
+public class PaymentService
+{
+    private readonly IPaymentProcessor _processor;
+
+    public PaymentService(IPaymentProcessor processor)
+    {
+        _processor = processor;
+    }
+    
+    public void MakePayment(decimal amount)
+    {
+        _processor.ProcessPayment(amount);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IPaymentProcessor creditCardprocessor = new CreditCard();
+        PaymentService paymentService = new PaymentService(creditCardprocessor);
+        
+        paymentService.MakePayment(100); // Output: Credit card payment processed for 100 $
+        
+        IPaymentProcessor paypalProcessor = new PaypalProcessor();
+        paymentService = new PaymentService(paypalProcessor);
+        
+        paymentService.MakePayment(150); // Output: PayPal payment processed for 150 $
+    }
+}
