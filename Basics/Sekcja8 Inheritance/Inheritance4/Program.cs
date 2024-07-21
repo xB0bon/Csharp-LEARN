5b574d7c54cc2244d7575c372544c501d57986fd
xB0bon <pan.bobon@gmail.com>
2024-07-21 11:00:15 +0200

    .

---
+++ Basics/Sekcja8 Inheritance/Inheritance4/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Inheritance4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        dog.MakeSound();
+        cat.MakeSound();
+    }
+}
+
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal makes a sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Cat meows");
+    }
+}
