f017f9e08878ada4c599768bea8ed095a84baaed
xB0bon <pan.bobon@gmail.com>
2024-07-22 22:45:45 +0200

    .

---
+++ Basics/Sekcja9/E3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        File.AppendAllText("log.txt", "Hello, world!" + "\n");
+        Console.WriteLine("Hello, World!");
+    }
+}
