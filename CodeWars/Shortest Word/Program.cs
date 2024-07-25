8d537a48f93b8cbcf10c37e4da5af974700545d8
xB0bon <pan.bobon@gmail.com>
2024-07-25 16:28:30 +0200

    .

---
+++ CodeWars/Shortest Word/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Shortest_Word;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("");
+    }
+    public static int FindShortestWordLength(string sentence)
+    {
+        string[] words = sentence.Split();
+        int shortestLength = words[0].Length;
+        foreach (var word in words)
+        {
+            if (word.Length < shortestLength)
+            {
+                shortestLength = word.Length;
+            }
+        }
+        return shortestLength;
+    }
+}
+
