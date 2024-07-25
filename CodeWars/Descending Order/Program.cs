8726ff33e30813e7ddece7ec4191db09bffdb132
xB0bon <pan.bobon@gmail.com>
2024-07-25 16:55:10 +0200

    25/07/2024

---
+++ CodeWars/Descending Order/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Descending_Order;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine();
+    }
+    
+    public static int DescendingOrder(int num)
+    {
+        string numStr = num.ToString();
+        char[] charArray = numStr.ToCharArray();
+        Array.Sort(charArray, (a,b) => b.CompareTo(a));
+        string result = new string(charArray);
+        return int.Parse(result);
+    }
+}
