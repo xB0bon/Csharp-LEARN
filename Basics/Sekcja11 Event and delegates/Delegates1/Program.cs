3d059e76b2bb371bd593ff7073f803ca2622d1a8
xB0bon <pan.bobon@gmail.com>
2024-07-25 13:10:14 +0200

    .

---
+++ Basics/Sekcja11 Event and delegates/Delegates1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Delegates1;
+
+class Program
+{
+
+    private delegate void Notify(string message);
+    
+    static void Main(string[] args)
+    {
+        Notify notifyDelegate = ShowMessage;
+        notifyDelegate("Hello Delegates!");
+    }
+
+    static void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
