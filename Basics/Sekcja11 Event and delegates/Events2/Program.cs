4f26c1c4b30b2b315ee470c8c9515c1e65cec574
xB0bon <pan.bobon@gmail.com>
2024-07-25 20:22:24 +0200

    25/07/2024

---
+++ Basics/Sekcja11 Event and delegates/Events2/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Events2;
+
+public delegate void TemperatureChangeHandler(string message);
+
+public class TemperatureMonitor
+{
+    public event TemperatureChangeHandler OnTemperatureChanged;
+    private int _temperature;
+    public int Temperature
+    {
+        get { return _temperature;}
+        set
+        {
+            _temperature = value;
+            if (_temperature > 30)
+            {
+                RaiseTemperatureChangedEvent($"Temperature is too high: {_temperature}");
+            }
+            else
+            {
+                RaiseTemperatureChangedEvent($"Temperature has been updated: {_temperature}");
+            }
+        } 
+    }
+
+    protected virtual void RaiseTemperatureChangedEvent(string message)
+    {
+        OnTemperatureChanged?.Invoke(message);
+    }
+}
+
+public class TemperatureAlert
+{
+    public void OnTemperatureChanged(string message)
+    {
+        Console.WriteLine("Alert: " + message);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        TemperatureMonitor monitor = new TemperatureMonitor();
+        TemperatureAlert alert = new TemperatureAlert();
+        
+        monitor.OnTemperatureChanged += alert.OnTemperatureChanged;
+
+        Console.WriteLine("Please enter a temperature: ");
+        int temperature = int.Parse(Console.ReadLine());
+        monitor.Temperature = temperature;
+    }
+}
