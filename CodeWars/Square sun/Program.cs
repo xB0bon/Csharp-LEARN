1c271a772faba6c7689921940eeb6a254821087b
xB0bon <pan.bobon@gmail.com>
2024-07-25 16:38:45 +0200

    25/07/2024

---
+++ CodeWars/Square sun/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Square_Sun;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        Console.WriteLine($"The sum of squares of the numbers is: {SquareSum(numbers)}");
+    }
+    
+    public static int SquareSum(int[] numbers)
+    {
+        int result = 0;
+        foreach (var number in numbers)
+        {
+            result += number * number;
+        }
+        
+        return result;
+    }
+}
