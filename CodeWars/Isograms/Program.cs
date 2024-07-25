3f616461c826d8db6168a1437974c58b36b7d133
xB0bon <pan.bobon@gmail.com>
2024-07-25 22:29:54 +0200

    25/07/2024

---
+++ CodeWars/Isograms/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Isograms;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine(IsIsogram("aaa"));
+    }
+    
+    public static bool IsIsogram(string str)
+    {
+        List<string> letters = new List<string>();
+        str = str.ToLower().Trim();
+        // Code on you crazy diamond!
+        foreach (var i in str)
+        {
+            if (letters.Exists(word => word == i.ToString()))
+            {
+                return false;
+            }
+            else
+            {
+                letters.Add(i.ToString());
+            }
+        }
+        return true;
+    }
+    
+    
+}
