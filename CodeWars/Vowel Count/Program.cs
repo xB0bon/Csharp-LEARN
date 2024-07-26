9458e44bd8930ff3acdfd6cdd3bb9126239cf266
xB0bon <pan.bobon@gmail.com>
2024-07-26 20:27:48 +0200

    26/07/2024

---
+++ CodeWars/Vowel Count/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Vowel_Count;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public static int GetVowelCount(string str)
+    {
+        int vowelCount = 0;
+
+        char[] vowels = new char[5] { 'a', 'e', 'i', 'o', 'u' };
+
+        foreach (var i in str)
+        {
+            if (vowels.Contains(i))
+            {
+                vowelCount++;
+            }
+        }
+        return vowelCount;
+    }
+}
