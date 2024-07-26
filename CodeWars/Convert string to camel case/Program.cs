88841ff24d62f29efb40bd21f6d9e91749363790
xB0bon <pan.bobon@gmail.com>
2024-07-26 19:35:48 +0200

    26/07/2024

---
+++ CodeWars/Convert string to camel case/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Convert_string_to_camel_case;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(ToCamelCase("the-stealth-warrior"));
+    }
+    
+    public static string ToCamelCase(string str)
+    {
+        char[] separators = new char[] { '-', '_' };
+        str = str.ToLower();
+        string[] splited = str.Split(separators);
+
+        for (int i = 0; i < splited.Length; i++)
+        {
+            if (i == 0)
+            {
+                continue;
+            }
+            else
+            {
+                splited[i] = char.ToUpper(splited[i][0]) + splited[i].Substring(1);
+            }
+        }
+        return string.Join("", splited);
+    }
+}
