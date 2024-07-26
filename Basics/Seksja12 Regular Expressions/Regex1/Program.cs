4468fc9bfe5c174069cc0025ccdecf5502789149
xB0bon <pan.bobon@gmail.com>
2024-07-26 12:29:01 +0200

    26/07/2024

---
+++ Basics/Seksja12 Regular Expressions/Regex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Regex1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string pattern = @"\d";
+        Regex regex = new Regex(pattern);
+
+        string text = "My number is 123";
+        
+        MatchCollection matchCollection = regex.Matches(text);
+        
+        
+        foreach (Match match in matchCollection)
+        {
+            GroupCollection group = match.Groups;
+            Console.WriteLine($"Found1: {group[0].Value}");
+            Console.WriteLine($"Found2: {match.Value}");
+        }
+
+        Console.WriteLine(matchCollection.Count + " hits found in: '" + text + "'");
+    }
+}
