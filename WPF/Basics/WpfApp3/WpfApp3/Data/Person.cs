b6c66add19687b3bb65280f2cce85931c8ac7f8c
xB0bon <pan.bobon@gmail.com>
2024-08-05 20:03:51 +0200

    udemy

---
+++ WPF/Basics/WpfApp3/WpfApp3/Data/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3.Data
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+    }
+
+    
+}
---
+++ WPF/Basics/WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp3.Data;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person = new Person("John", 18);
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = person;
+        }
+
+        private void ShowInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Name : {person.Name} \n Age : {person.Age}");
+        }
+    }
+}
